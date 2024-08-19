90e20b9ea1006ece3a5e68282c52a6431f52955f
edwin <edwinhendly17@gmail.com>
2024-08-19 11:30:01 +0700

    create: other functionality

--- script.js
+++ script.js
@@ -1,40 +1,51 @@
 const randomizeBtn = document.querySelector("#randomize");
 const randomNumberBox = document.querySelector("#random-number");
 const refreshSeatsEl = document.querySelector("#refresh-seats");
-const leftSeatsEl = document.querySelector('.left-seats .seats');
-const rightSeatsEl = document.querySelector('.right-seats .seats');
+const leftSeatsEl = document.querySelector(".left-seats .seats");
+const rightSeatsEl = document.querySelector(".right-seats .seats");
 
 const leftSeats = 20;
 const rightSeats = 15;
-const totalSeat = leftSeats + rightSeats; // 1 to 35
+const totalSeats = leftSeats + rightSeats; // 1 to 35
 const keyPrefix = "PPTI22_ARRADIUS";
 
-function createSeats(numberOfSeats, positionEl, startAt){
-    for(let i = 0; i < numberOfSeats; i++){
-        const el = document.createElement('div');
-        el.classList.add("seat");
-        el.dataset.number = startAt + i;
-        el.innerHTML = startAt + i;
-        positionEl.append(el);
-    }
+function createSequenceOfNumbers(start, numbers) {
+	return new Array(numbers).fill(0).map((_, i) => i + start);
 }
 
-function highlightOccupiedSeats(){
-    const data = JSON.parse(localStorage.getItem(`${keyPrefix}_RANDOMIZED`) ?? '[]');
-    data.forEach(number => {
-        const seatEl = document.querySelector(`.seat[data-number='${number}']`);
-        seatEl?.classList.add('seat-occupied');
-    });
+function createSeats(numberOfSeats, positionEl, startAt) {
+	for (let i = 0; i < numberOfSeats; i++) {
+		const el = document.createElement("div");
+		el.classList.add("seat");
+		el.dataset.number = startAt + i;
+		el.innerHTML = startAt + i;
+		positionEl.append(el);
+	}
 }
 
-function initializeSeats(){
-    createSeats(leftSeats, leftSeatsEl, 1);
-    createSeats(rightSeats, rightSeatsEl, leftSeats + 1);
-    highlightOccupiedSeats();
+function highlightOccupiedSeats() {
+	const data = JSON.parse(
+		localStorage.getItem(`${keyPrefix}_RANDOMIZED`) ?? "[]"
+	);
+	data.forEach((number) => {
+		const seatEl = document.querySelector(`.seat[data-number='${number}']`);
+		seatEl?.classList.add("seat-occupied");
+		removeNumberFromArray(number);
+	});
+}
+
+function initializeSeats() {
+	createSeats(leftSeats, leftSeatsEl, 1);
+	createSeats(rightSeats, rightSeatsEl, leftSeats + 1);
+	highlightOccupiedSeats();
 }
 
 function generateRandom(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function generateRandomFromArray(array) {
+	return array[~~(Math.random() * array.length)];
 }
 
 function setLocalStorage(item) {
@@ -63,7 +74,7 @@ async function shuffle() {
 				resolve();
 				return;
 			}
-			let random = generateRandom(1, totalSeat);
+			let random = generateRandom(1, totalSeats);
 			randomNumberBox.innerHTML = random;
 			timeElapsed += 50;
 		}, 50);
@@ -73,16 +84,34 @@ async function shuffle() {
 function occupySeat(number) {
 	const seat = document.querySelector(`.seat[data-number='${number}']`);
 	seat.classList.add("seat-occupied");
+	removeNumberFromArray(number);
 }
 
+function removeNumberFromArray(number) {
+	numbers = numbers.filter((e) => e != number);
+}
 
-// flow
+function checkFull(){
+    const data = JSON.parse(
+		localStorage.getItem(`${keyPrefix}_RANDOMIZED`) ?? "[]"
+	);
 
+    return data.length == totalSeats;
+}
+
+// initialization
+const numbers = createSequenceOfNumbers(1, totalSeats);
 initializeSeats();
 randomizeBtn.addEventListener("click", async function () {
+
+    if(checkFull()) {
+        alert("Full Seats!! Please Reset!!");
+        return;
+    };
+
 	let randomNumber;
 	do {
-		randomNumber = generateRandom(1, totalSeat);
+		randomNumber = generateRandomFromArray(numbers);
 	} while (checkForExistence(randomNumber));
 
 	await shuffle();
